982b04eac00b1c46106c983f8cacf0123f570dc1
Maxim Zakharov <maxiiim.256@gmail.com>
2025-04-17 01:05:46 +0300

    дз 5

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -1,3 +1,8 @@
 function hideSelf() {
-  // ваш код...
+    const button = document.querySelector('.hide-self-button');
+
+    button.addEventListener('click', () => {
+      button.setAttribute('hidden',true); 
+      button.hidden = true;
+    });
 }
--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,35 @@
 function initCarousel() {
-  // ваш код...
+  const leftArrow = document.querySelector('.carousel__arrow_left');
+  const rightArrow = document.querySelector('.carousel__arrow_right');
+  const currentSlide = document.querySelector('.carousel__inner');
+  const elem = document.querySelector('.carousel');
+  let index = 0;
+  const slideWidth = currentSlide.offsetWidth; 
+
+
+  leftArrow.style.display = 'none';
+
+
+  elem.addEventListener('click', () => {
+    if (index === 0) {
+      leftArrow.style.display = 'none';
+    } else if (index === 3) {
+      rightArrow.style.display = 'none';
+    } else {
+      leftArrow.style.display = '';
+      rightArrow.style.display = '';
+    }
+  });
+
+
+  leftArrow.addEventListener('click', () => {
+    index -= 1; 
+    currentSlide.style.transform = `translateX(${index * -slideWidth}px)`; 
+  });
+
+
+  rightArrow.addEventListener('click', () => {
+    index += 1; 
+    currentSlide.style.transform = `translateX(${index * -slideWidth}px)`; 
+  });
 }
